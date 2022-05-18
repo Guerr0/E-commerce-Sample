1116519d46d1c922a284397560eec012572867e2
Guerr0 <frencyapple@gmail.com>
2022-05-18 15:49:23 +0200

    Aggiunta funzionolità acquista e componente cart

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -11,7 +12,7 @@ import { Product, products } from '../products';
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute ,  private cartService: CartService) {}
 
   ngOnInit(): void {
      // First get the product id from the current route.
@@ -20,5 +21,11 @@ export class ProductDetailsComponent implements OnInit {
 
   // Find the product that correspond with the id provided in route.
   this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
   }
 }
